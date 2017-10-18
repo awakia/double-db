e44cbcc6ef6e81c3caf96057f76505906675d760
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2017-10-18 18:16:15 +0900

    Add tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,21 @@ RSpec.describe User, type: :model do
       expect(user.profile).to be_present
       expect(user.profile.location).to eq 'Tokyo'
       expect(user.profile.working_histories.size).to eq 2
+
+      u = User.includes(profile: :working_histories).to_a.last
+      expect(u.profile).to be_present
+      expect(u.profile.location).to eq 'Tokyo'
+      expect(u.profile.working_histories.size).to eq 2
+
+      u2 = User.preload(profile: :working_histories).to_a.last
+      expect(u2.profile).to be_present
+      expect(u2.profile.location).to eq 'Tokyo'
+      expect(u2.profile.working_histories.size).to eq 2
+
+      u3 = User.eager_load(profile: :working_histories).to_a.last
+      expect(u3.profile).to be_present
+      expect(u3.profile.location).to eq 'Tokyo'
+      expect(u3.profile.working_histories.size).to eq 2
     end
   end
 end
