012e16cda949016456f79df8ab8e812ac3737ffa
izumin5210 <masayuki@izumin.info>
2017-10-19 15:15:18 +0900

    Fix spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,10 +18,8 @@ RSpec.describe User, type: :model do
       expect(u2.profile.location).to eq 'Tokyo'
       expect(u2.profile.working_histories.size).to eq 2
 
-      u3 = User.eager_load(profile: :working_histories).to_a.last
-      expect(u3.profile).to be_present
-      expect(u3.profile.location).to eq 'Tokyo'
-      expect(u3.profile.working_histories.size).to eq 2
+      expect { User.eager_load(profile: :working_histories).to_a }.to raise_error(ActiveRecord::StatementInvalid)
+      expect { User.includes(:profile).where(profile: { location: 'Tokyo' }).to_a }.to raise_error(ActiveRecord::StatementInvalid)
     end
   end
 end
